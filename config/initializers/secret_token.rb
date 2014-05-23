b83b4770ba3f518a4e6c97a6e77e363820d19d50
Chris Allen <cehallen@gmail.com>
2014-05-23 13:43:56 -0400

    Second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QandA2::Application.config.secret_key_base = 'ecb858b370e7a42ef9c5869816c82b41a7828d56462e08dd5fc63214baaafb9216b797ee3af020759df7a2f5495f5a08f767007ba07b23dbe871f60a709059d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QandA2::Application.config.session_store :cookie_store, key: '_QandA2_session'
